ecf1bbd689ea1debe4ac273d1377985b101162df
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2024-12-10 14:44:33 -0300

    Criando rota que devolve informações do usuário e gerando JWT

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuhtRequest{
     email: string,
@@ -24,7 +25,24 @@ class AuthUserService{
             throw new Error("User/Password incorrect")
         }
 
-        return { okay: true }
+        const token = sign(
+            {
+            name: user.name, 
+            email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
